196a822562098eb79bf3cf3f0bb8c33e895ef866
Yurihpaiva <yurihamasaki88@gmail.com>
2023-03-09 11:46:23 -0300

    adicionado a aula28

---
+++ aula28/estoque/src/App.js
@@ -0,0 +1,40 @@
+import "./estilo.css";
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Lista() {
+
+  let cursos = [
+    "Marketing",
+    "Fullstack",
+    "Data Analytics"
+  ];
+
+  return (
+  <ul>
+    {/* forma mais didática e legível */}
+    {cursos.map(function(cada) {
+      return (<li> {cada} </li>);
+    })}
+
+    {/* forma mais abreviada do .map usando arrow function*/}
+    {cursos.map(x => <li>{x}</li>)}
+  </ul>
+  )
+}
+
+export default function App() {  
+  let nome = "Vanessa"
+return(
+<>
+  <header>
+    <Link/>Home
+  </header>
+  <h1>Olá mundo!</h1>
+  <h2 className="red">Digital College</h2>
+  <hr/>
+  <p>Bem vindo {nome}</p>
+  <Lista/>
+</>
+)  
+}
